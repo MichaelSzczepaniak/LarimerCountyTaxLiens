4b2930cde65ea8866d2db1509d981e58d2553dd8
MichaelSzczepaniak <mszczepaniak@pobox.com>
2015-06-27 11:59:31 -0600

    impl passing returnAmt as total or ROI

--- Rproj/effectiveMonths.R
+++ Rproj/effectiveMonths.R
@@ -1,16 +1,32 @@
 ## Calculates the effective months over which simple interest had been paid.
 ##
-## roi - Return on Investment for single tax lien, single value (scalar)
-## principals - vector containing the original principal paid for a tax
+## returnAmt - Return on Investment for a single tax lien; single value (scalar)
+## principals - Numeric vector containing the original principal paid for a tax
 ##              lien(p0) and all successive subtax payments(pj)
-## interest - annual interest rate (simple) on a tax lien, single value
-## delta - number of months between tax sale and subtax issuance
-##         For Larimer county, this value is 3 since tax sale is in November,
-##         but subtax issuance is in September. 
-effectiveMonths <- function(roi, principals, annint = 0.10, delta = 3) {
+## interest - Annual interest rate (simple) on a tax lien; single value (scalar)
+## delta - Number of months between tax sale and subtax issuance; single value
+##         (scalar). For Larimer county, this value is 3 since tax sale is 
+##         typically in November, but subtax issuance is typically in September.
+## returnIsTotal - FALSE if returnAmt is the ROI (difference between the total
+##                 amount returned and what was paid).
+##                 TRUE if returnAmt is the total amount returned. If this is
+##                 the case, the function will do an internal calculation for
+##                 ROI as part of calculating the effective months of interest.
+effectiveMonths <- function(returnAmt, principals, annint = 0.10, delta = 3,
+                            returnIsTotal = TRUE) {
     mint <- annint / 12  # monthly interest
-    roiByInt = roi / mint
+    if(returnIsTotal) {
+        # total amount returned has been passed, so ROI is difference
+        # between what was paid and what was returned to investor
+        roiByInt = (returnAmt - sum(principals)) / mint
+    }
+    else {
+        # actual ROI is passes
+        roiByInt = returnAmt / mint
+    }
+    
     if(length(principals) == 1) {
+        # no subtax has been paid, simple calculation
         return(roiByInt / principals[1])
     }
     # If we are here, one or more subtax payments were made.
